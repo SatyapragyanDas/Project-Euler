b6ebc81a13fa28b3da7c045fb0b0dc633c1410e3
SatyapragyanDas <53405743+SatyapragyanDas@users.noreply.github.com>
2019-08-05 22:19:14 +0530

    Create q6.java

---
+++ q6.java
@@ -0,0 +1,11 @@
+//q6
+class A{
+public static void main(String args[]){
+int n=100;
+int sum=(n*(n+1))/2;
+int squaresum=(((n*(n+1))/2)*((n*(n+1))/2));
+int squareofsum=sum*sum;
+System.out.println("square of total sum="+squareofsum+"  "+"sum of square="+squaresum);
+System.out.println(squareofsum-squaresum);
+}
+}
