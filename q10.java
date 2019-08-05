58ff2e09ee42f8265b90900874b147fa93f36e1f
SatyapragyanDas <53405743+SatyapragyanDas@users.noreply.github.com>
2019-08-05 22:23:10 +0530

    Create q10.java

---
+++ q10.java
@@ -0,0 +1,27 @@
+//q10
+import java.util.*;
+class A{
+public static void main(String[] args){
+int sum = 1;
+int ctr = 0; 
+int n = 0; 
+while (ctr < 2000000) {
+n++; 
+if (n % 2 != 0) { 
+if (is_Prime(n)) {
+ sum += n; 					
+ }
+}
+ctr++; 	
+}
+System.out.println("\nSum of the prime numbers till 100: "+sum); 	
+}
+public static boolean is_Prime(int n) {
+for (int i = 3; i * i <= n; i+= 2) {
+if (n % i == 0) {
+return false; 
+}
+}
+return true;
+}
+}
