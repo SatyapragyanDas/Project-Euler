548ec1e9071c75a3ec581ef80e52227a82e528fe
SatyapragyanDas <53405743+SatyapragyanDas@users.noreply.github.com>
2019-08-11 23:29:11 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -1,24 +1,14 @@
 //q3
 class A{
-public boolean isPrime(long m){
-long q=2;
-while(q<m){
-if(m%q==0)
-return false;
-q++;
-}
-return true;
-}
 public static void main(String args[]){
-A p=new A();
+int i=0;
 long n=600851475143l;
-long d=2;
-long temp;
-while(d<=n){
-temp=n%d;
-if((temp==0)&(p.isPrime(temp)))
-System.out.println(d+"  ");
-d++;
+for(i=2;i<=n;i++){
+if(n%i==0){
+n=n/i;
+i--;
 }
 }
-}  
+System.out.println(i);
+}
+}
