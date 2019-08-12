d9274ee227ce71d405754fe81539b90395f45ff4
SatyapragyanDas <53405743+SatyapragyanDas@users.noreply.github.com>
2019-08-12 13:02:35 +0530

    Update q10.java

--- q10.java
+++ q10.java
@@ -1,21 +1,27 @@
 //q10
 class A{
+public static boolean isPrime(long n) {
+
+        if((n > 2 && n % 2 == 0)){
+            return false;
+        }
+
+        for (long i = 3; i <= (long)Math.sqrt(n); i += 2) {
+
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 public static void main(String args[]){
-int i=0,j=0;
-int sum=2;
-for(i=2;i<2000000;i++){
-int p=0;
-	if(i%2!=0){
-		for(j=2;j<=Math.sqrt(i);j++){
-			if(i%j==0){
-			p=1;
-			break;
-			}
-		}
-	if(p!=1){
-		sum=sum+i;
-	}
-	}
+long n=3;
+long sum=2;
+while(n<2000000){
+if(isPrime(n))
+sum=sum+n;
+n++;
 }
 System.out.println(sum);
 }
