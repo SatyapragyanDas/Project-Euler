cb2866a2298ad7b3e98cb2c8607bc35221e03c51
SatyapragyanDas <53405743+SatyapragyanDas@users.noreply.github.com>
2019-08-12 10:15:37 +0530

    Update q6.java

--- q6.java
+++ q6.java
@@ -3,7 +3,7 @@ class A{
 public static void main(String args[]){
 int n=100;
 int sum=(n*(n+1))/2;
-int squaresum=(((n*(n+1))/2)*((n*(n+1))/2));
+int squaresum=(n*(n+1)*(2*n+1))/6;
 int squareofsum=sum*sum;
 System.out.println("square of total sum="+squareofsum+"  "+"sum of square="+squaresum);
 System.out.println(squareofsum-squaresum);
