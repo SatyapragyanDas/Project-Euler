be790ce859f215d8e9c2c699258cb0b9d9ac4ecd
SatyapragyanDas <53405743+SatyapragyanDas@users.noreply.github.com>
2019-08-12 11:08:54 +0530

    Update q7.java

--- q7.java
+++ q7.java
@@ -1,28 +1,25 @@
 //q7
 class A{
+public static boolean isPrime(int n){
+for(int i=2;i<n;i++){
+if(n%i==0)
+return false;
+}
+return true;
+}
 public static void main (String args[]){		
-       int i =0;
-       int num =0;
-       int count=0;
-       String  primeNumbers = "";
-       for (i = 1; i <= 10001; i++)         
-       { 		  	  
-          int counter=0; 	  
-          for(num =i; num>=1; num--)
-	  {
-             if(i%num==0)
-	     {
- 		counter = counter + 1;
-	     }
-	  }
-	  if (counter ==2)
-	  {
-	     primeNumbers = primeNumbers + i + " ";
-	  }
-	count++;
-	if(count==10001)
-	break;	
-       }	
-       System.out.println("Prime numbers from 1 to 10001 are :"+primeNumbers);
-   }
+int n=2;
+int x=0;
+int counter=0;
+while(true){
+if(counter==10001)
+break;
+if(isPrime(n)){
+x=n;
+counter++;
+}
+n++;
+}
+System.out.println(x);
+}
 }
