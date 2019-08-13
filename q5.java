a93df7b1a727d1371d4d73db355d8b7488d02811
SatyapragyanDas <53405743+SatyapragyanDas@users.noreply.github.com>
2019-08-13 22:41:52 +0530

    Update q5.java

--- q5.java
+++ q5.java
@@ -1,3 +1,5 @@
+//q5
+import java.util.*;
 class A{
 	static long gcd(long x,long y){
 		if(y==0)
@@ -5,14 +7,20 @@ class A{
 		else 
 		return gcd(y,x%y);
 	} 
-	static long findLcm(long[] a){
+	static long findLcm(int[] a,int n){
 	long p=a[0];
-	for(int i=1;i<20;i++)
+	for(int i=1;i<n;i++)
 	p=((a[i]*p)/(gcd(a[i],p)));
 	return p;
 	} 
 public static void main(String args[]){
-long arr[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-System.out.println(findLcm(arr)); 
+int arr[]=new int[50];
+System.out.println("Enter array length=");
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for(int i=0;i<n;i++){
+arr[i]=(i+1);
+}
+System.out.println(findLcm(arr,n)); 
 }
 }
