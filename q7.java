7ac76e414a5370652bf4c498c59d46a51088f7fe
SatyapragyanDas <53405743+SatyapragyanDas@users.noreply.github.com>
2019-08-13 22:11:25 +0530

    Update q7.java

--- q7.java
+++ q7.java
@@ -1,15 +1,20 @@
 //q7
 class A{
-public static boolean isPrime(int n){
-for(int i=2;i<n;i++){
-if(n%i==0)
+public static boolean isPrime(long n){
+if((n>2 && n%2==0)){
 return false;
+}
+for (long i = 3; i <= (long)Math.sqrt(n); i += 2) {
+if (n % i == 0) {
+return false;
+}
 }
 return true;
 }
+
 public static void main (String args[]){		
-int n=2;
-int x=0;
+long n=2;
+long x=0;
 int counter=0;
 while(true){
 if(counter==10001)
